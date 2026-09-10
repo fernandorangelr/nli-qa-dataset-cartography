37f1fb980aab27b6e535aa55d063efbe38936bfc
Fernando Rangel <rangel.juanfernando@gmail.com>
2026-09-09 19:36:59 -0600

    Fixed bugs with ANLI dataset fields

--- run.py
+++ run.py
@@ -143,7 +143,12 @@ def main():
         dataset_id = tuple(args.dataset.split(':')) if args.dataset is not None else \
             default_datasets[args.task]
         # MNLI has two validation splits (one with matched domains and one with mismatched domains). Most datasets just have one "validation" split
-        eval_split = 'validation_matched' if dataset_id == ('glue', 'mnli') else 'validation'
+        if dataset_id == ('glue', 'mnli'):
+            eval_split = 'validation_matched'
+        elif dataset_id == ('facebook/anli',):
+            eval_split = 'test_r1'
+        else:
+            eval_split = 'validation'
         # Load the raw data
         dataset = datasets.load_dataset(*dataset_id)
 
